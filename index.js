51410aacce4c783973b4d09039c9048f09fc5166
Lisa Lee <snickers495@gmail.com>
2018-03-27 16:42:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const ageIsValid = Number.isInteger(currentAge)
 const randomNumber = Math.random(0, 20)
 const randomInteger = Math.floor(randomNumber)
 const randomUserID = 1000000001 + randomInteger
-const newId = randomUserID 
+const newId = randomUserID + oldID
